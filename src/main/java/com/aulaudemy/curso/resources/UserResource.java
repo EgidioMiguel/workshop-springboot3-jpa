af5f749afeda1d26c24035f20d6c046e3c8f213c
Miguel Egidio <miguelegidiojava@gmail.com>
2025-07-16 10:29:00 -0300

    Service layer, component registration

--- src/main/java/com/aulaudemy/curso/resources/UserResource.java
+++ src/main/java/com/aulaudemy/curso/resources/UserResource.java
@@ -1,22 +1,40 @@
 package com.aulaudemy.curso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aulaudemy.curso.entities.User;
+import com.aulaudemy.curso.services.UserService;
 
 @RestController //Combina @Controller e @ResponseBody. Retorna JSON diretamente.
 @RequestMapping(value = "/users") //Mapeia qualquer tipo de requisição para métodos ou classes.
 public class UserResource {
-
+	
+	@Autowired
+	private UserService service;
 	
 	//ResponseEntity<T> exclusivo do spring, serve para retorna resposta da web
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "1234");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		
+		List<User> list =  service.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		
+		User obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj);
+		
 	}
 	
 }
